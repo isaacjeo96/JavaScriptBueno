4a8e65d11f8c0d651fff0e13f4b8c3efc3f217a8
isaacjeo96 <isaacjeo96@outlook.com>
2021-10-28 23:28:22 -0500

    JavaScript

---
+++ PL/Controllers/UsuarioController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PL.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        //
+        // GET: /Usuario/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetAll()
+        {
+            return View();
+        }
+	}
+}
---
+++ PL/Scripts/Usuario/UsuarioCRUD.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+    GetAll();
+});
+
+function GetAll() {
+    $.ajax({
+        type: 'GET',
+        url: 'https://randomuser.me/api/',
+        success: function (result) {//200 OK 
+            $('#tblUsuario tbody');
+             
+                var filas =
+                  '<tr>'
+                + "<td class='text-center'>" + result.results[0].gender + "</td>"
+                + "<td class='text-center'>" + result.results[0].name.title + " " + result.results[0].name.first + " " + result.results[0].name.last + "</td>"
+                + "<td class='text-center'>" + result.results[0].location.street.number + result.results[0].location.street.name + "</td>"
+                + "<td class='text-center'>" + result.results[0].email + "</td>"
+                + "<td>"+"<img src= "+result.results[0].picture.large +">"+ "</td>"
+                + "</tr>";
+                $("#tblUsuario tbody").append(filas);
+       
+        },
+        error: function (result) {
+        alert('Error en la consulta.' + result.responseJSON.ErrorMessage);
+        }
+    });
+}
